4684f39dd28e6921cc632a64ba12bd1fb66f99b3
knvs97 <knvs97@gmail.com>
2018-07-04 21:38:43 +0530

    Changed Config Object into hashMap

--- src/main/com/zoho/dpaas/comm/executor/conf/ContextType.java
+++ src/main/com/zoho/dpaas/comm/executor/conf/ContextType.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Setter @Getter @JsonIgnoreProperties(ignoreUnknown = true)
 public class ContextType {
     private String name;
     private Integer min;
     private Integer max;
     @JsonProperty(value = "configs")
-    private Config configs;
+    private Map<String,String> configs;
 }
